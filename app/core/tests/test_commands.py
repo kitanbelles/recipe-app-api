baf5370773f9a7fdfe5e7d8c45d093080b761034
Olakitan Aladesuyi <kitanaladesuyi@gmail.com>
2022-10-25 23:11:02 +0100

    Add command to wait for db

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,17 @@
+"""
+test custom django management command 
+"""
+
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Pyscopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+class CommandTest (SimpleTestCase):
+    """test commands."""
+    
+    def test_waitfor_db_ready(self):
+        """Test waiting for database if database is ready"""
