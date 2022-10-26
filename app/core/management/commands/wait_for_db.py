a5537c0682f6f3291bf2854294c176fda83292ad
Olakitan Aladesuyi <kitanaladesuyi@gmail.com>
2022-10-26 10:32:24 +0100

    Format whole app with black

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -4,6 +4,7 @@ Django command to wait for database to be available
 
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to wait for database"""
 
