52d3e7df9c58c4c3c2d906939b4a38a964294a21
Olakitan Aladesuyi <kitanaladesuyi@gmail.com>
2022-10-27 23:34:24 +0100

    Add User model Add tests for user model and fields normalize email address

--- app/app/settings.py
+++ app/app/settings.py
@@ -85,7 +85,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -128,3 +127,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "core.User"
